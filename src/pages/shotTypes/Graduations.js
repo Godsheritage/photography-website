e9b3999994400bce2ad2ad3fa03dbd3ffdc3d9ac
Godsheritage <adeoyegodsheritage@gmail.com>
2024-02-22 18:07:32 -0500

    worked on the portfolio page

--- src/pages/shotTypes/Graduations.js
+++ src/pages/shotTypes/Graduations.js
@@ -1,8 +1,8 @@
-import {motion} from "framer-motion"
-import React, {useContext} from 'react'
-import { transition1 } from '../../transitions';
-import { CursorContext } from '../../context/CursorContext';
-
+import { motion } from "framer-motion";
+import React, { useContext } from "react";
+import { transition1 } from "../../transitions";
+import { CursorContext } from "../../context/CursorContext";
+import studioShot from "../../img/Studio/studio.jpeg";
 
 const Graduations = () => {
   const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
@@ -17,11 +17,94 @@ const Graduations = () => {
       <div
         onMouseEnter={mouseEnterHandler}
         onMouseLeave={mouseLeaveHandler}
-        className="container mx-auto h-full relative"
+        className=" container mx-auto h-full relative overflow-y-scroll"
       >
-    </div>
-    </motion.section>
-  )
-}
+        {/* image boxes */}
+        <div className="container mx-auto px-4 lg:px-0">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+            <div className="relative">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-full w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
 
-export default Graduations
+            <div className="relative">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-96 w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+            <div className="relative col-span-2 md:col-span-1 lg:col-span-2">
+              <img
+                className="object-cover h-96 sm:h-80 lg:h-full w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+            <div className="relative col-span-2">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-full w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+            <div className="relative col-span-2">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-96 w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+            <div className="relative">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-full w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+            <div className="relative">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-full w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+            <div className="relative">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-full w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+            <div className="relative">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-full w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+            <div className="relative">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-full w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+            <div className="relative col-span-2">
+              <img
+                className="object-cover h-64 sm:h-80 lg:h-full w-full hover:scale-110 transition-all duration-500"
+                src={studioShot}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Graduations;
